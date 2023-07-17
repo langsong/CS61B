995bade5418b8c621421f65e263f9a67f3e2603b
langsong <will0216sl@gmail.com>
2023-07-18 01:13:39 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,18 +5,19 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if(n%2 == 1){
+            /** the case for n is odd */
+            return 3*n + 1;
+        }else{
+            /** the case for n is even */
+            return n/2;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
+        /** the sequence terminates at 1 */
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
